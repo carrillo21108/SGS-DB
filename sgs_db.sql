129156d567dfa5747c46467bd273d0ef32d764f9
carrillo21108 <87512111+carrillo21108@users.noreply.github.com>
2023-04-13 11:55:28 -0600

    Tabla Especialidad, Medico y SP para la creacion de medicos

--- sgs_db.sql
+++ sgs_db.sql
@@ -35,11 +35,42 @@ CREATE TABLE Paciente(
 		FOREIGN KEY (no_paciente_madre) REFERENCES Paciente(no_paciente)
 );
 
+CREATE TABLE Especialidad(
+	id_especialidad SERIAL,
+	nombre VARCHAR(20) NOT NULL,
+	
+	PRIMARY KEY(id_especialidad)
+);
+
+CREATE TABLE Medico(
+	no_colegiado VARCHAR(20) NOT NULL,
+	cui VARCHAR(20) UNIQUE NOT NULL,
+	id_especialidad INT NOT NULL,
+	usuario VARCHAR(10) UNIQUE NOT NULL,
+	clave VARCHAR(10) NOT NULL,
+	
+	PRIMARY KEY(no_colegiado),
+	CONSTRAINT fk_persona
+		FOREIGN KEY (cui) REFERENCES Persona(cui),
+	CONSTRAINT fk_especialidad
+		FOREIGN KEY (id_especialidad) REFERENCES Especialidad(id_especialidad)
+);
+
 CREATE OR REPLACE PROCEDURE createPaciente(cui VARCHAR(20),nombre VARCHAR(50),apellidos VARCHAR(60),direccion VARCHAR(100),telefono VARCHAR(10),id_centro_medico VARCHAR(5))
 AS $BODY$
 BEGIN
     INSERT INTO Persona VALUES(cui,nombre,apellidos,direccion,telefono,id_centro_medico);
 	INSERT INTO Paciente(cui) VALUES (cui);
+END;
+$BODY$
+LANGUAGE plpgsql;
+
+CREATE OR REPLACE PROCEDURE createMedico(cui VARCHAR(20),nombre VARCHAR(50),apellidos VARCHAR(60),direccion VARCHAR(100),telefono VARCHAR(10),
+										 id_centro_medico VARCHAR(5),no_colegiado VARCHAR(20),id_especialidad INT,usuario VARCHAR (10), clave VARCHAR(10))
+AS $BODY$
+BEGIN
+    INSERT INTO Persona VALUES(cui,nombre,apellidos,direccion,telefono,id_centro_medico);
+	INSERT INTO Medico VALUES (no_colegiado,cui,id_especialidad,usuario,clave);
 END;
 $BODY$
 LANGUAGE plpgsql;
